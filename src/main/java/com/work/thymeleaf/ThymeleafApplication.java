e49c6a24ca74c4376f4764470c786cc51004929e
AleksandrShcherbakov <Shcherbakov89@gmail.com>
2018-11-27 19:35:59 +0300

    ne nugno

--- src/main/java/com/work/thymeleaf/ThymeleafApplication.java
+++ src/main/java/com/work/thymeleaf/ThymeleafApplication.java
@@ -1,5 +1,8 @@
 package com.work.thymeleaf;
 
+import com.sun.nio.zipfs.ZipPath;
+import com.work.thymeleaf.controller.ImageController;
+import com.work.thymeleaf.helper.Helper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +12,7 @@ import java.net.MalformedURLException;
 public class ThymeleafApplication {
 
 	public static void main(String[] args) throws MalformedURLException {
+
 
 		SpringApplication.run(ThymeleafApplication.class, args);
 	}
